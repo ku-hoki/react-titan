ee1e61d3c4cb2b9383bdc3ff6f4c18da559ac512
kutts <kutts858@gmail.com>
2025-04-26 01:13:09 +0600

    Конечные две страницы

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Footer() {
     return (
         <section className='section-4'>
-                <div className='wrapper-4'>
-                    <div className="logo">
-                        <img width={305} height={108} src="/img/logo.png" alt="Логотип компании ТИТАН" />
-                    </div>
-
-                    <nav className='nav-links'>
-                        <a href='#' className='nav-link active'>Основной сайт</a>
-                        <a href='#'>Тестирования</a>
-                        <a href='#'>Мои результаты</a>
-                        <a href='#'>Администартору</a>
-                    </nav>
+            <div className='wrapper-4'>
+                <div className="logo">
+                    <img width={305} height={108} src="/img/logo.png" alt="Логотип компании ТИТАН" />
                 </div>
-            </section>
+
+                <nav className='nav-links'>
+                    <Link to='/'>
+                        <a href='#' className='nav-link active'>Основной сайт</a>
+                    </Link>
+                    <a href='#'>Тестирования</a>
+                    <a href='#'>Мои результаты</a>
+                    <a href='#'>Администартору</a>
+                </nav>
+            </div>
+        </section >
     );
 }
 
